1a2a93bd7e55e7370ac53f7a0bdd1b88072a81a4
rockrockrockwong <1016923@163.com>
2019-03-13 00:43:53 +0800

    forkjoin testing

---
+++ javaStudy/src/localTest/threads/forkjoin/ForkJoin_GO.java
@@ -0,0 +1,13 @@
+package localTest.threads.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoin_GO {
+
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool;
+    }
+
+
+}
